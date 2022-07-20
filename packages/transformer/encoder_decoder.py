3b60ddea1dedfc517862c114722b8296d2b12ace
smfsamir <fsamir@nvidia.com>
2022-07-20 12:43:00 -0400

    Change batch to second dimension when calling self-attention

--- packages/transformer/encoder_decoder.py
+++ packages/transformer/encoder_decoder.py
@@ -93,7 +93,8 @@ class EncoderLayer(nn.Module):
 
     def forward(self, x, mask):
         "Follow Figure 1 (left) for connections."
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, attn_mask=mask)) 
+        # x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, attn_mask=mask)) 
+        x = self.sublayer[0](x, lambda x: self.self_attn(x.transpose(1,0), x.transpose(1,0), x.transpose(1,0), attn_mask=mask)) 
         return self.sublayer[1](x, self.feed_forward)
 
 class PositionwiseFeedForward(nn.Module):
