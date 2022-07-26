Correct ntokens computation for padding different batches

main_cora_gat.py
@@ -27,7 +27,7 @@ def run_train_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleIn
         ntokens += subgraph_bundle.ntokens 
     elapsed = time.time() - start
     print(f"Train loss on epoch: {total_loss / ntokens}; time taken: {elapsed}")
-    return total_loss / ntokens
+    return total_loss / ntokens, elapsed
 
 def run_eval_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleInput], model: EncoderDecoder, \
                     loss_compute: SimpleLossCompute):
@@ -59,7 +59,6 @@ def eval_accuracy(graph_bundle: TransformerGraphBundleInput, model: EncoderDecod
     return test_accuracy
 
 def train_model():
-    tb_sw = SummaryWriter()
 
     data = citegrh.load_cora()
     features = data.features.clone().detach().to('cuda')
@@ -98,15 +97,19 @@ def train_model():
         drop_last=True,
         num_workers=0, 
         device=device)
-    num_subgraphs = 4
+    num_subgraphs = 3
+    best_loss_epoch = 0
 
+    tb_log_dir = f"runs/batch-{batch_size}_num_sg-{num_subgraphs}/"
+    tb_sw = SummaryWriter(tb_log_dir)
     for nepoch in range(nepochs):
         model.train()
         nbatches = train_nids.shape[0] // batch_size
-        epoch_loss = run_train_epoch(cora_data_gen(train_dataloader, nbatches, num_subgraphs, features, labels, device), model, 
+        epoch_loss, train_epoch_elapsed  = run_train_epoch(cora_data_gen(train_dataloader, nbatches, num_subgraphs, features, labels, device), model, 
             SimpleLossCompute(model.generator, criterion, model_opt))
         
         tb_sw.add_scalar('Loss/train', epoch_loss, nepoch)
+        tb_sw.add_scalar('Duration/train', train_epoch_elapsed, nepoch)
         
         model.eval()
         with torch.no_grad():
@@ -116,28 +119,21 @@ def train_model():
             if validation_loss < best_loss:
                 checkpoint_model(model)
                 best_loss = validation_loss
+                best_loss_epoch = nepoch
             
+    print(f"Best validation epoch: {best_loss_epoch}")
     load_model(model) # mutation
     model.eval()
     with torch.no_grad():
         test_labels = labels[test_nids]
         test_acc = eval_accuracy(test_cora_data_gen(graph.adj().to_dense().cuda() + torch.eye(adj.shape[0], device='cuda'), features, test_nids, test_labels, device), model)
         tb_sw.add_scalar('Accuracy/test', test_acc)
+    print(f"{test_acc:.3f},{best_loss:.3f},{best_loss_epoch}")
 
-def visualize_jacobian():
-    features, labels, train_mask, _, _, adj = load_cora_data()
-    model = make_model(features.shape[1], len(labels.unique()) + 1, N=2).cuda() # +1 for the padding index, though I don't think it's necessary.
-    load_model(model) # mutation
-    jacobianized_model = JacobianGAT(model, 0, 1, cora_data_gen(features, labels, train_mask, adj))
-    visualize_influence(jacobianized_model, features[0])
 
 def main(args):
     if args.train_model:
         train_model()
-    elif args.visualize_jacobian:
-        visualize_jacobian()
-    elif args.test_dataloader_dgl:
-        test_dataloader_dgl()
 
 if __name__ == "__main__":
     parser = ArgumentParser()

packages/transformer/data.py
@@ -11,7 +11,7 @@ from packages.utils.sp_utils import convert_scipy_sparse_to_torch, select_submat
 
 class TransformerGraphBundleInput:
     "Object for holding a minibatch of data with mask during training."
-    def __init__(self, src_feats: torch.Tensor, trg_labels: torch.Tensor, src_mask: torch.Tensor, train_inds: torch.Tensor, device: str):
+    def __init__(self, src_feats: torch.Tensor, trg_labels: torch.Tensor, src_mask: torch.Tensor, train_inds: torch.Tensor, ntokens: int, device: str):
         """Object for holding required (simple) graph data.
 
         Args:
@@ -25,7 +25,7 @@ class TransformerGraphBundleInput:
         self.src_mask = src_mask
 
         self.trg_labels = trg_labels.to(device) 
-        self.ntokens = trg_labels.shape[0] * trg_labels.shape[1] # number of labelled tokens in the batch. TODO: need to think about this. How will the minibatch updating work?
+        self.ntokens = ntokens # number of labelled tokens in the batch. TODO: this is WRONG when we do padding. we should pass it in explicitly.
         self.train_inds = train_inds
 
 def retrieve_features_for_minibatch(batch_global_inds, all_features):
@@ -47,10 +47,12 @@ def retrieve_labels_for_minibatch(global_output_node_inds: torch.Tensor, all_lab
     """
     return all_labels[global_output_node_inds] 
 
+# TODO: shouldn't be necessary to convert back to dense. I was being lazy.
 def convert_mfg_to_sg_adj(mfg: DGLBlock, square_shape: int, device: str):
     sparse_adj = mfg.adj()
     square_adj = torch.sparse_coo_tensor(sparse_adj._indices(), sparse_adj._values(), size=(square_shape, square_shape), device=device) 
     return square_adj.to_dense()
+    # return square_adj
 
 def construct_batch(target_nodes, subgraph_nodes, mfgs, all_features, all_labels, device):
     first_layer_mfg = mfgs[0]
@@ -69,7 +71,7 @@ def construct_batch(target_nodes, subgraph_nodes, mfgs, all_features, all_labels
     minibatch_labels = all_labels[target_nodes].unsqueeze(0)
     output_node_inds = output_node_inds.unsqueeze(0)
 
-    minibatch = TransformerGraphBundleInput(all_minibatch_feats, minibatch_labels, minibatch_adjacencies, output_node_inds, device)
+    minibatch = TransformerGraphBundleInput(all_minibatch_feats, minibatch_labels, minibatch_adjacencies, output_node_inds, output_node_inds.shape[1], device)
     return minibatch
 
 def pad_graph_bundle(graph_bundle: TransformerGraphBundleInput, device: str) -> None: # WARNING: mutates graph bundle object
@@ -89,7 +91,8 @@ def stack_graph_bundles(graph_bundles: List[TransformerGraphBundleInput]) -> Tra
     src_feats = torch.cat([graph_bundle.src_feats for graph_bundle in graph_bundles])
     trg_labels = torch.cat([graph_bundle.trg_labels for graph_bundle in graph_bundles])
     train_inds = torch.cat([graph_bundle.train_inds for graph_bundle in graph_bundles])
-    return TransformerGraphBundleInput(src_feats, trg_labels, src_masks, train_inds, 'cuda')
+    ntokens = sum([graph_bundle.ntokens for graph_bundle in graph_bundles])
+    return TransformerGraphBundleInput(src_feats, trg_labels, src_masks, train_inds, ntokens, 'cuda')
 
 # TODO: this needs to change for packing batches. reduce the number of loops
 def cora_data_gen(dataloader: dgl.dataloading.DataLoader, 
@@ -131,7 +134,7 @@ def cora_data_gen(dataloader: dgl.dataloading.DataLoader,
 
 def test_cora_data_gen(adj: torch.Tensor, features: torch.Tensor, test_nids: torch.Tensor, labels: torch.Tensor, device: str):
     adj_mat_layerwise = adj.expand(2,-1,-1) 
-    return TransformerGraphBundleInput(features.unsqueeze(0), labels.unsqueeze(0), adj_mat_layerwise.unsqueeze(0), test_nids.unsqueeze(0), device)
+    return TransformerGraphBundleInput(features.unsqueeze(0), labels.unsqueeze(0), adj_mat_layerwise.unsqueeze(0), test_nids.unsqueeze(0), test_nids.shape[0], device)
 
 def load_cora_data():
     data = citegrh.load_cora()