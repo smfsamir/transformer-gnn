e0e92502a495197be7b6397bda20c583409b1aa6
smfsamir <fsamir@nvidia.com>
2022-08-01 12:00:27 -0400

    functional implementation of training on reddit dataset

--- main_reddit_gat.py
+++ main_reddit_gat.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import dgl
 import numpy as np
@@ -7,6 +8,8 @@ import torch
 from typing import Iterator
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import LambdaLR
+from typing import List
+from sklearn.metrics import f1_score
 
 from packages.data_management.reddit_data import get_dataloader, load_reddit_adj, load_reddit_feats, load_reddit_labels, load_reddit_masks
 from packages.transformer.optimizer import rate, SimpleLossCompute
@@ -14,14 +17,6 @@ from packages.utils.checkpointing import load_model, checkpoint_model
 from packages.transformer.data import cora_data_gen, TransformerGraphBundleInput
 from packages.transformer.encoder_decoder import make_model, EncoderDecoder
 
-# NOTE: loading the dataset requires a significant amount of memory (30G+) for some reason; possibly because the dataset is loaded with an inefficient adjacency matrix. The COO format requires less than 1G. 
-# dataset = dgl.data.RedditDataset()
-# graph = dataset[0]
-
-# adj_sparse = graph.adj(scipy_fmt='coo') 
-# train_ids = np.arange(adj_sparse.shape[0])[graph.ndata['train_mask']]
-# graph = dgl.graph((adj_sparse.row, adj_sparse.col))  
-# dataset = None
 
 def run_train_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleInput], model: EncoderDecoder, loss_compute: SimpleLossCompute,
                     optimizer: torch.optim.Optimizer, scheduler: LambdaLR):
@@ -35,6 +30,7 @@ def run_train_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleIn
                             subgraph_bundle.train_inds) # B x B_out x model_D.  
         # TODO: need to think about this loss computation carefully. Is it even possible?
         loss, loss_node = loss_compute(out, subgraph_bundle.trg_labels, subgraph_bundle.ntokens)
+
         ntokens += subgraph_bundle.ntokens 
         total_loss += loss
         loss_node.backward()
@@ -60,29 +56,36 @@ def run_eval_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleInp
     print(f"Validation loss on epoch: {total_loss / ntokens}")
     return total_loss / ntokens 
 
-# TODO: need to think about this carefully. We'll be evaluating on packed batches.
+# TODO: need to think about this carefully if we use B > 1. We'll be evaluating on packed batches later.
 def eval_accuracy(subggraph_bundle_generator: Iterator[TransformerGraphBundleInput], model: EncoderDecoder):
     total = 0
     num_correct = 0 
+    predictions = []
+    test_labels = []
     for graph_bundle in subggraph_bundle_generator:
         out = model.forward(graph_bundle.src_feats, graph_bundle.src_mask, graph_bundle.train_inds) # B x B_out x model_D.  
         out = model.generator(out) # B x num_nodes x num_classes
         out = out.squeeze(0) # num_nodes x num_classes
         out = out.argmax(axis=1) # num_nodes
         mb_test_labels = graph_bundle.trg_labels.squeeze(0)
-        total += mb_test_labels.shape[0]
-        num_correct += (out == mb_test_labels).sum()
-        test_accuracy = num_correct / total
-        print(test_accuracy)
-    return test_accuracy
+        # total += mb_test_labels.shape[0]
+        # num_correct += (out == mb_test_labels).sum()
+        predictions.append(out)
+        test_labels.append(mb_test_labels)
+    predictions = torch.cat(predictions)
+    test_labels = torch.cat(test_labels)
+    # test_accuracy = num_correct / total
+    test_f1 = f1_score(test_labels.cpu(), predictions.cpu(), average='micro')
+    return test_f1
 
-def build_dataloader(graph: dgl.DGLHeteroGraph, sampler: dgl.dataloading.MultiLayerNeighborSampler, ids: torch.tensor):
+def build_dataloader(graph: dgl.DGLHeteroGraph, bs: int, fanouts: List[int], ids: torch.tensor):
+    sampler = dgl.dataloading.MultiLayerNeighborSampler(fanouts)
     dataloader = dgl.dataloading.DataLoader(
         graph, ids, sampler,
-        batch_size=64,
-        shuffle=False,
-        drop_last=False,
-        num_workers=0)
+        batch_size=bs,
+        shuffle=True,
+        drop_last=True
+    )
     return dataloader
 
 def train_model(bs: int, num_sg: int):
@@ -99,16 +102,15 @@ def train_model(bs: int, num_sg: int):
     num_classes = len(labels.unique())
     all_ids = np.arange(masks.shape[1])
     train_ids = all_ids[masks[0,:]]
-    val_ids = all_ids[masks[0,:]]
-    test_ids = all_ids[masks[0,:]]
+    val_ids = all_ids[masks[1,:]]
+    test_ids = all_ids[masks[2,:]]
 
     graph = dgl.graph((adj_sparse.row, adj_sparse.col))
-    sampler = dgl.dataloading.MultiLayerNeighborSampler([5, 5])
-    _build_dataloader = partial(build_dataloader, graph, sampler)
+    _build_dataloader = partial(build_dataloader, graph, bs, [5,5])
 
     train_dataloader = _build_dataloader(train_ids)
     val_dataloader = _build_dataloader(val_ids)
-    test_dataloader = _build_dataloader(test_ids)
+    # test_dataloader = _build_dataloader(test_ids)
 
     criterion = CrossEntropyLoss(reduction='sum').cuda()
     model = make_model(feats.shape[1], num_classes, N=2).cuda() 
@@ -132,22 +134,60 @@ def train_model(bs: int, num_sg: int):
 
     device= 'cuda'
     nepochs = 30
+    best_loss = float('inf')
     for nepoch in range(nepochs):
         model.train()
-        nbatches = train_ids.shape[0] // batch_size
-        epoch_loss, train_epoch_elapsed  = run_train_epoch(cora_data_gen(train_dataloader, nbatches, num_subgraphs, feats, labels, device), model, 
-            SimpleLossCompute(model.generator, criterion), optimizer, lr_scheduler)
+        train_nbatches = train_ids.shape[0] // batch_size
+        epoch_loss, train_epoch_elapsed  = run_train_epoch(cora_data_gen(train_dataloader, train_nbatches, num_subgraphs, feats, labels, device), model, SimpleLossCompute(model.generator, criterion), optimizer, lr_scheduler)
 
         tb_sw.add_scalar('Loss/train', epoch_loss, nepoch)
         tb_sw.add_scalar('Duration/train', train_epoch_elapsed, nepoch)
         
         model.eval()
+        val_nbatches = val_ids.shape[0] // batch_size 
         with torch.no_grad():
-            validation_loss = run_eval_epoch(cora_data_gen(val_dataloader, nbatches, 1, feats, labels, device), model, 
-                SimpleLossCompute(model.generator, criterion, None))
+            validation_loss = run_eval_epoch(cora_data_gen(val_dataloader, val_nbatches, num_subgraphs, feats, labels, device), model, 
+                SimpleLossCompute(model.generator, criterion))
             tb_sw.add_scalar('Loss/validation', validation_loss, nepoch)
             if validation_loss < best_loss:
                 checkpoint_model(model)
                 best_loss = validation_loss
                 best_loss_epoch = nepoch
-train_model(32, 1)
+
+def evaluate_model(bs, num_subgraphs):
+    adj_sparse = load_reddit_adj()
+    feats = load_reddit_feats()
+    masks = load_reddit_masks()
+    labels = load_reddit_labels()
+    num_classes = len(labels.unique())
+    all_ids = np.arange(masks.shape[1])
+    test_ids = all_ids[masks[2,:]]
+
+    graph = dgl.graph((adj_sparse.row, adj_sparse.col))
+    _build_dataloader = partial(build_dataloader, graph, bs, [5,5])
+    test_dataloader = _build_dataloader(test_ids)
+    model = make_model(feats.shape[1], num_classes, N=2).cuda() 
+    load_model(model) # mutation
+    model.eval()
+    device = 'cuda'
+    with torch.no_grad():
+        test_nbatches = test_ids.shape[0] // bs 
+        test_acc = eval_accuracy(cora_data_gen(test_dataloader, test_nbatches, num_subgraphs, feats, labels, device), model)
+    print(f"{test_acc:.3f}")
+
+
+
+def main(args):
+    if args.evaluate_model:
+        evaluate_model(args.bs, args.num_sg)
+    elif args.train_model:
+        train_model(args.bs, args.num_sg)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bs", type=int)
+    parser.add_argument("num_sg", type=int)
+    parser.add_argument("--train_model", action='store_true')
+    parser.add_argument("--evaluate_model", action='store_true')
+
+    main(parser.parse_args())
