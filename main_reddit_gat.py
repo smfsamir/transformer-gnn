664b9f9c9cfebe23e6310996b65a086a520e7bec
smfsamir <fsamir@nvidia.com>
2022-08-03 13:55:56 -0400

    DDP-ify module for training on reddit

--- main_reddit_gat.py
+++ main_reddit_gat.py
@@ -1,9 +1,13 @@
 import argparse
+import torch.multiprocessing as mp
 import time
 import dgl
 import numpy as np
+import torch.distributed as dist
+import os
 from functools import partial
 from torch.nn import CrossEntropyLoss
+from torch.nn.parallel import DistributedDataParallel as DDP
 import torch
 from typing import Iterator
 from torch.utils.tensorboard import SummaryWriter
@@ -26,7 +30,7 @@ def run_train_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleIn
     ntokens = 0
     for subgraph_bundle in subgraph_bundle_generator: 
         optimizer.zero_grad(set_to_none=True)
-        out = model.forward(subgraph_bundle.src_feats, subgraph_bundle.src_mask,  
+        out = model(subgraph_bundle.src_feats, subgraph_bundle.src_mask,  
                             subgraph_bundle.train_inds) # B x B_out x model_D.  
         # TODO: need to think about this loss computation carefully. Is it even possible?
         loss, loss_node = loss_compute(out, subgraph_bundle.trg_labels, subgraph_bundle.ntokens)
@@ -46,7 +50,7 @@ def run_eval_epoch(subgraph_bundle_generator: Iterator[TransformerGraphBundleInp
     total_loss = 0
     ntokens = 0
     for subgraph_bundle in subgraph_bundle_generator: 
-        out = model.forward(subgraph_bundle.src_feats, subgraph_bundle.src_mask,  
+        out = model(subgraph_bundle.src_feats, subgraph_bundle.src_mask,  
                             subgraph_bundle.train_inds) # B x B_out x model_D.  
         # TODO: need to think about this loss computation carefully. Is it even possible?
         loss, _ = loss_compute(out, subgraph_bundle.trg_labels, subgraph_bundle.ntokens)
@@ -63,8 +67,8 @@ def eval_accuracy(subggraph_bundle_generator: Iterator[TransformerGraphBundleInp
     predictions = []
     test_labels = []
     for graph_bundle in subggraph_bundle_generator:
-        out = model.forward(graph_bundle.src_feats, graph_bundle.src_mask, graph_bundle.train_inds) # B x B_out x model_D.  
-        out = model.generator(out) # B x num_nodes x num_classes
+        out = model(graph_bundle.src_feats, graph_bundle.src_mask, graph_bundle.train_inds) # B x B_out x model_D.  
+        out = model.module.generator(out) # B x num_nodes x num_classes
         out = out.squeeze(0) # num_nodes x num_classes
         out = out.argmax(axis=1) # num_nodes
         mb_test_labels = graph_bundle.trg_labels.squeeze(0)
@@ -88,7 +92,7 @@ def build_dataloader(graph: dgl.DGLHeteroGraph, bs: int, fanouts: List[int], ids
     )
     return dataloader
 
-def train_model(bs: int, num_sg: int):
+def train_model(model, gpu):
     """Train the model on Reddit data.
 
     Args:
@@ -99,24 +103,22 @@ def train_model(bs: int, num_sg: int):
     feats = load_reddit_feats()
     masks = load_reddit_masks()
     labels = load_reddit_labels()
-    num_classes = len(labels.unique())
     all_ids = np.arange(masks.shape[1])
     train_ids = all_ids[masks[0,:]]
     val_ids = all_ids[masks[1,:]]
-    test_ids = all_ids[masks[2,:]]
+    device = gpu
 
     graph = dgl.graph((adj_sparse.row, adj_sparse.col))
     fanouts = [5,5]
+    batch_size = 32
+    bs = batch_size
     _build_dataloader = partial(build_dataloader, graph, bs, fanouts)
 
     train_dataloader = _build_dataloader(train_ids)
     val_dataloader = _build_dataloader(val_ids)
     # test_dataloader = _build_dataloader(test_ids)
 
-    criterion = CrossEntropyLoss(reduction='sum').cuda()
-    model = make_model(feats.shape[1], num_classes, N=2).cuda() 
-
-    torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-6)
+    criterion = CrossEntropyLoss(reduction='sum').to(gpu)
 
     optimizer = torch.optim.Adam(
         model.parameters(), lr=1.0, betas=(0.9, 0.98), eps=1e-9
@@ -127,20 +129,18 @@ def train_model(bs: int, num_sg: int):
             step, 512, factor=1, warmup=3000
         ),
     )
-    batch_size = bs
-    num_subgraphs = num_sg
+    num_subgraphs = 1
 
     tb_log_dir = f"runs/batch-{batch_size}_num_sg-{num_subgraphs}_reddit"
     tb_sw = SummaryWriter(tb_log_dir)
 
-    device= 'cuda'
     nepochs = 30
     best_loss = float('inf')
     max_graph_padding = batch_size + batch_size * fanouts[0] + (batch_size + batch_size * fanouts[0]) * fanouts[1] 
     for nepoch in range(nepochs):
         model.train()
         train_nbatches = train_ids.shape[0] // batch_size
-        epoch_loss, train_epoch_elapsed  = run_train_epoch(cora_data_gen(train_dataloader, train_nbatches, num_subgraphs, feats, labels, max_graph_padding, device), model, SimpleLossCompute(model.generator, criterion), optimizer, lr_scheduler)
+        epoch_loss, train_epoch_elapsed  = run_train_epoch(cora_data_gen(train_dataloader, train_nbatches, num_subgraphs, feats, labels, max_graph_padding, device), model, SimpleLossCompute(model.module.generator, criterion), optimizer, lr_scheduler)
 
         tb_sw.add_scalar('Loss/train', epoch_loss, nepoch)
         tb_sw.add_scalar('Duration/train', train_epoch_elapsed, nepoch)
@@ -149,7 +149,7 @@ def train_model(bs: int, num_sg: int):
         val_nbatches = val_ids.shape[0] // batch_size 
         with torch.no_grad():
             validation_loss = run_eval_epoch(cora_data_gen(val_dataloader, val_nbatches, num_subgraphs, feats, labels, max_graph_padding, device), model, 
-                SimpleLossCompute(model.generator, criterion))
+                SimpleLossCompute(model.module.generator, criterion))
             tb_sw.add_scalar('Loss/validation', validation_loss, nepoch)
             if validation_loss < best_loss:
                 checkpoint_model(model)
@@ -180,19 +180,40 @@ def evaluate_model(bs, num_subgraphs):
         test_acc = eval_accuracy(cora_data_gen(test_dataloader, test_nbatches, num_subgraphs, feats, labels, max_graph_padding, device), model)
     print(f"{test_acc:.3f}")
 
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = 'localhost'
+    os.environ["MASTER_PORT"] = '12355'
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
+def get_input_output_dims():
+    feats = load_reddit_feats()
+    labels = load_reddit_labels()
+    num_classes = len(labels.unique())
+    return feats.shape[1], num_classes
 
-def main(args):
-    if args.evaluate_model:
-        evaluate_model(args.bs, args.num_sg)
-    elif args.train_model:
-        train_model(args.bs, args.num_sg)
+def main_proc(rank, world_size):
+    setup(rank, world_size)
+    input_dim, output_num_classes = get_input_output_dims()
+    model = make_model(input_dim, output_num_classes, N=2).to(rank) 
+    model = DDP(model, device_ids=[rank], output_device=rank, find_unused_parameters=False)
+    train_model(model, rank)
+
+def main_global(args):
+    world_size = args.num_gpus
+    mp.spawn(main_proc, args=(world_size, ), nprocs = world_size, join=True)
+
+    # if args.evaluate_model:
+    #     evaluate_model(args.bs, args.num_sg)
+    # elif args.train_model:
+    #     train_model(args.bs, args.num_sg)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("bs", type=int)
-    parser.add_argument("num_sg", type=int)
-    parser.add_argument("--train_model", action='store_true')
-    parser.add_argument("--evaluate_model", action='store_true')
+    # parser.add_argument("bs", type=int)
+    # parser.add_argument("num_sg", type=int)
+    # parser.add_argument("--train_model", action='store_true')
+    # parser.add_argument("--evaluate_model", action='store_true')
+    parser.add_argument("num_gpus", type=int)
+    main_global(parser.parse_args())
 
-    main(parser.parse_args())
+    # main(parser.parse_args())
