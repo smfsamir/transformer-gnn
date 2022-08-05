313dea130072f029c18b56acc53e32235175852e
smfsamir <fsamir@nvidia.com>
2022-08-05 11:11:48 -0400

    Put model on cuda for visualizing reddit

--- main_visualize_reddit.py
+++ main_visualize_reddit.py
@@ -21,7 +21,7 @@ masks = load_reddit_masks()
 labels = load_reddit_labels()
 graph = dgl.graph((adj_sparse.row, adj_sparse.col))
 
-model = make_model(feats.shape[1], len(torch.tensor(labels).unique()), N=2)
+model = make_model(feats.shape[1], len(torch.tensor(labels).unique()), N=2).cuda()
 load_ddp_model(model, path="checkpoints")
 all_ids = np.arange(masks.shape[1])
 train_ids = all_ids[masks[0,:]]
